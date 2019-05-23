8a00b5e34c4cd1d9d458c63703c29667d43cb93f
Luiz Santos <luizcsbh@gmail.com>
2019-05-23 17:30:37 -0300

    Update facebook.strategy.js

--- configs/facebook.strategy.js
+++ configs/facebook.strategy.js
@@ -2,8 +2,8 @@ const passport = require('passport');
 const  FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new FacebookStrategy({
-    clientID: '699470520515686',
-    clientSecret: '62f343c249cfe6b56fce4d2d9d268c58',
+    clientID: '2425549260823264',
+    clientSecret: 'ff43c57cf0aae4dad56cc68ebe51dc72',
     callbackURL: "http://localhost:3000/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
