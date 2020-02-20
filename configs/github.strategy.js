db89190e321544c7c47d798de3412201283a6019
Luiz Santos <luizcsbh@gmail.com>
2020-02-20 10:40:27 -0300

    Update github.strategy.js

--- configs/github.strategy.js
+++ configs/github.strategy.js
@@ -3,7 +3,7 @@ const GitHubStrategy = require('passport-github').Strategy;
 
 passport.use(new GitHubStrategy({
     clientID: 'Iv1.8da0b13938edd642',
-    clientSecret: '0a9348317478c90b404d7a64af792062a078bcc',
+    clientSecret: '0a9348317478c90b404d7a64af792062a078bcc9',
     callbackURL: "https://pwntrabalhofinal.herokuapp.com/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
