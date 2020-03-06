887304b6a5da473b8a3f07ab52790eae864c8e85
Luiz Santos <luizcsbh@gmail.com>
2020-03-06 19:17:23 -0300

    Update Connection.js

--- store/Connection.js
+++ store/Connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 // const connectionString = 'mongodb+srv://pwn:1234!!!!@cluster0-8zacm.mongodb.net/test?retryWrites=true'
-const connectionString = 'mongodb+srv://userTeste:4018388@cluster0-jiwbo.mongodb.net/favoritos?retryWrites=true'
+//const connectionString = 'mongodb+srv://userTeste:4018388@cluster0-jiwbo.mongodb.net/favoritos?retryWrites=true'
+
+const connectionString = 'mongodb+srv://appmongodb:adminapp@cluster0-khuil.mongodb.net/favoritos?retryWrites=true'
 
 const openConnection = () => mongoose.connect(connectionString)
 
